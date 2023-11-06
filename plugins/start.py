03f4efd36aa66e90ef53ea2718f89310ece35262
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-06 20:44:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,7 +16,7 @@ from config import (
     START_MSG,
 )
 #from database.sql import add_user, delete_user, full_userbase, query_msg
-from database.mongo import collection, adds_user, del_user, fulls_userbase, present,user
+from database.mongo import collection, adds_user, del_user, fulls_userbase, present_user
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
