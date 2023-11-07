a28c1ca02ab02b2a006688d3cb89fabe56444700
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-08 02:21:34 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -10,63 +10,64 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("batch"))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
+            # Prompt the user to provide the first message from the DB Channel
             first_message = await client.ask(
                 text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                timeout=60,
+                timeout=60
             )
-        except BaseException:
-            return
+        except:
+            return  # Return if there's an exception (e.g., timeout)
+
+        # Get the message ID from the provided message or link
         f_msg_id = await get_message_id(client, first_message)
+
         if f_msg_id:
             break
-        await first_message.reply(
-            "❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel",
-            quote=True,
-        )
-        continue
+        else:
+            # Inform the user of an error if the message/link is not from the DB Channel
+            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
 
     while True:
         try:
+            # Prompt the user to provide the last message from the DB Channel
             second_message = await client.ask(
                 text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                timeout=60,
+                timeout=60
             )
-        except BaseException:
-            return
+        except:
+            return  # Return if there's an exception (e.g., timeout)
+
+        # Get the message ID from the provided message or link
         s_msg_id = await get_message_id(client, second_message)
+
         if s_msg_id:
             break
-        await second_message.reply(
-            "❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel",
-            quote=True,
-        )
-        continue
+        else:
+            # Inform the user of an error if the message/link is not from the DB Channel
+            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
 
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-                )
-            ]
-        ]
-    )
-    await second_message.reply_text(
-        f"<b>Here is your link</b>\n\n{link}",
-        quote=True,
-        reply_markup=reply_markup,
-    )
+    # Generate a list of links for each message between the first and second message
+    message_links = []
+    for msg_id in range(min(f_msg_id, s_msg_id), max(f_msg_id, s_msg_id) + 1):
+        string = f"get-{msg_id * abs(client.db_channel.id)}"
+        base64_string = await encode(string)
+        link = f"https://t.me/{client.username}?start={base64_string}"
+        message_links.append(link)
+
+    # Send the generated links to the user
+    for link in message_links:
+        await message.reply(f"Here is a link for one of the messages:\n{link}")
+
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("genlink"))
