e64923412c19c56f01a484877a6534b3250626fd
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-17 14:49:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -269,12 +269,11 @@ async def not_joined(client: Bot, message: Message):
         disable_web_page_preview=True,
     )
 
+WAIT_MSG = """"<b>Processing ...</b>"""
 
-@Bot.on_message(filters.command(["users", "stats"]) & filters.user(ADMINS))
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(
-        chat_id=message.chat.id, text="<code>Processing ...</code>"
-    )
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
