5af9b94f9fcc41076c5cabf9eb0bfc8a1f8da8e2
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-06 01:52:36 +0530

    Create useless.py

---
+++ plugins/useless.py
@@ -0,0 +1,8 @@
+
+@client.on(events.NewMessage(chats=events.PeerUser()))
+async def forward_to_admin(event):
+    # Check if the message is from a private chat
+    if event.is_private:
+        # Forward the user's message to the admin
+        await client.send_message(ADMIN_ID, f"User ID: {event.sender_id}\nMessage: {event.raw_text}")
+
