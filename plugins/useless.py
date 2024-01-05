bd2bbf84f89a340bd7eea2ffa97365f6429a94ee
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-06 01:55:19 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,23 @@
+import os
 
-
-
-
+from bot import Bot
+from config import (
+    ADMINS,
+    API_HASH,
+    APP_ID,
+    CHANNEL_ID,
+    #DB_URI, sql database
+    MONGO_URI,
+    FORCE_MSG,
+    FORCE_SUB_CHANNEL,
+    FORCE_SUB_GROUP,
+    LOGGER,
+    PROTECT_CONTENT,
+    START_MSG,
+    TG_BOT_TOKEN,
+)
+from pyrogram import filters
+from pyrogram.types import Message
 
 
 
