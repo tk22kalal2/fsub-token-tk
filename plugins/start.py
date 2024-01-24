230fcda59b11f11520ff4712159a57b98cc4b040
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-25 02:48:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -248,7 +248,7 @@ async def start_command(client: Bot, message: Message):
                 mention=message.from_user.mention,
                 id=message.from_user.id,
             ),
-            reply_markup=buttonz,
+            reply_markup=InlineKeyboardMarkup(out),
             disable_web_page_preview=True,
             quote=True,
         )
