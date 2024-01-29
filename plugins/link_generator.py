cef6d582217ad61c203475c47ae0336a608c0436
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-29 14:14:20 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -55,28 +55,18 @@ async def batch(client: Client, msg: Message):
             await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
             continue
 
-    if bool(CUSTOM_CAPTION) and bool(msg.caption):
-        caption = CUSTOM_CAPTION.format(
-            previouscaption=msg.caption.html if msg.caption else "",
-            filename=msg.document.file_name,
-        )
-    else:
-        caption = msg.caption.html if msg.caption else ""
-
     # Generate a list of links for each message between the first and second message
     message_links = []
     for msg_id in range(min(f_msg_id, s_msg_id), max(f_msg_id, s_msg_id) + 1):
         string = f"get-{msg_id * abs(client.db_channel.id)}"
         base64_string = await encode(string)
-        link = f"https://t.me/{client.username}?start={base64_string}"
-        caption = msg.caption.html if msg.caption else ""
-        message_links.append(caption)
+        link = f"https://t.me/{client.username}?start={base64_string}"                
         message_links.append(link)
     
 
     # Send the generated links to the user
     for link in message_links:
-        await msg.reply(f"{caption}\n{link}")
+        await msg.reply(f"here is your link \n{link}")
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("genlink"))
