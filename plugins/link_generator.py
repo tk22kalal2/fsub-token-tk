96f8e192d0a68a5a306ec5b467a45657d046aea8
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-29 14:54:13 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -60,14 +60,15 @@ async def batch(client: Client, msg: Message):
     for msg_id in range(min(f_msg_id, s_msg_id), max(f_msg_id, s_msg_id) + 1):
         string = f"get-{msg_id * abs(client.db_channel.id)}"
         base64_string = await encode(string)
-        link = f"https://t.me/{client.username}?start={base64_string}"
+        link = f"https://telegram.me/{client.username}?start={base64_string}"
         msg = await client.get_messages(client.db_channel.id, msg_id)
         caption = msg.caption.html if msg.caption else ""
         message_links.append((link, caption))
 
     # Send the generated links with captions to the user
-    for link, caption in message_links:                
-        await msg.reply(f"{caption} \n{link}, parse_mode=ParseMode.HTML)        
+    for link, caption in message_links:
+        f_caption=f"{caption}
+        await msg.reply(f"⏯{f_caption} \n{link}, parse_mode=ParseMode.HTML)        
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("genlink"))
 async def link_generator(client: Client, message: Message):
