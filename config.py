29f7448b13fed4a267f4df65f5b970e9b9a83c19
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-02 02:40:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,25 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv("config.env")
 
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
+DB_URI = environ.get("DB_URI", "")
+DB_NAME = environ.get("DB_NAME", "vjbotz")
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# Bot Information
+
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # your bot username without @
+PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+
+# Auto Delete Information
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+
+
 # Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
