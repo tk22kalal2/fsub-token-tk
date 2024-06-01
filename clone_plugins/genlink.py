338a606a11a5529bf39ca1e779e46dcc407cc448
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-02 05:23:48 +0530

    Update genlink.py

--- clone_plugins/genlink.py
+++ clone_plugins/genlink.py
@@ -8,77 +8,37 @@ from plugins.database import unpack_new_file_id
 from clone_plugins.users_api import get_user, get_short_link
 import base64
 
-import asyncio
-import base64
-import re
-import requests
-
-from pyrogram import filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import FloodWait
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-
-from config import ADMINS
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+@Client.on_message(filters.command(['link', 'plink']))
+async def gen_link_s(client: Client, message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply('Reply to a message to get a shareable link.')
+    file_type = replied.media
+    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
+        return await message.reply("Reply to a supported media")
+    if message.has_protected_content:
+        return await message.reply("okDa")
 
-async def encode(string):
-    string_bytes = string.encode("ascii")
-    base64_bytes = base64.urlsafe_b64encode(string_bytes)
-    base64_string = (base64_bytes.decode("ascii")).strip("=")
-    return base64_string
-
-async def decode(base64_string):
-    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
-    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string = string_bytes.decode("ascii")
-    return string
-
-
-async def get_messages(client, message_ids):
-    messages = []
-    total_messages = 0
-    while total_messages != len(message_ids):
-        temb_ids = message_ids[total_messages : total_messages + 200]
-        try:
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id, message_ids=temb_ids
-            )
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id, message_ids=temb_ids
-            )
-        except BaseException:
-            pass
-        total_messages += len(temb_ids)
-        messages.extend(msgs)
-    return messages
-
-
-async def get_message_id(client, message):
-    if (
-        message.forward_from_chat
-        and message.forward_from_chat.id == client.db_channel.id
-    ):
-        return message.forward_from_message_id
-    elif message.forward_from_chat or message.forward_sender_name or not message.text:
-        return 0
-    else:
-        pattern = "https://t.me/(?:c/)?(.*)/(\\d+)"
-        matches = re.match(pattern, message.text)
-        if not matches:
-            return 0
-        channel_id = matches.group(1)
-        msg_id = int(matches.group(2))
-        if channel_id.isdigit():
-            if f"-100{channel_id}" == str(client.db_channel.id):
-                return msg_id
-        elif channel_id == client.db_channel.username:
-            return msg_id
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+    
+    file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
+    string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
+    string += file_id
+    outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
+    user_id = message.from_user.id
+    user = await get_user(user_id)
+    # Get the bot's username
+    bot_username = (await client.get_me()).username
+    share_link = f"https://t.me/{bot_username}?start={outstr}"
+    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
+    short_link = await get_short_link(user, share_link)
+    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
