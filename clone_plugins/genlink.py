1f39a44c34a18ba7cfc4ed1848b86281a0110ebd
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-03 18:03:24 +0530

    Update genlink.py

--- clone_plugins/genlink.py
+++ clone_plugins/genlink.py
@@ -49,6 +49,47 @@ logger.setLevel(logging.INFO)
     )
 )
 async def channel_post(bot, message):
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
+    try:
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except Exception as e:
+        LOGGER(__name__).warning(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
+    converted_id = post_message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ]
+        ]
+    )
+
+    await reply_text.edit(
+        f"<b>Here is your link</b>\n\n{link}",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+    )
+
+    if not DISABLE_CHANNEL_BUTTON:
+        try:
+            await post_message.edit_reply_markup(reply_markup)
+        except Exception:
+            pass
+            
 @Client.on_message(filters.command(['link', 'plink']))
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
