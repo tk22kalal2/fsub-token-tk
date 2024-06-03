1edf139a477a832eaf3d52a3ac4f820231ea615d
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-03 18:28:57 +0530

    Update genlink.py

--- clone_plugins/genlink.py
+++ clone_plugins/genlink.py
@@ -31,6 +31,57 @@ logger.setLevel(logging.INFO)
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+@Client.on_message(filters.command(['link']))
+async def gen_link_s(client: Client, message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply('Reply to a message to get a shareable link.')
+    file_type = replied.media
+    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
+        return await message.reply("Reply to a supported media")
+    if message.has_protected_content:
+        return await message.reply("okDa")
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
+    try:
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(
+            chat_id=client.db_channel.id, disable_notification=True
+        )
+    except Exception as e:
+        LOGGER(__name__).warning(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
+    converted_id = post_message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ]
+        ]
+    )
+
+    await reply_text.edit(
+        f"<b>Here is your link</b>\n\n{link}",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+    )
+
+    if not DISABLE_CHANNEL_BUTTON:
+        try:
+            await post_message.edit_reply_markup(reply_markup)
+        except Exception:
+            pass
+        
 @Client.on_message(
     filters.private
     & filters.user(ADMINS)
