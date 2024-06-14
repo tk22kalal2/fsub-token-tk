7b094ed26f2f976f4b335d79a3aba1d587428395
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-14 18:27:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,14 +4,12 @@
 
 
 class script(object):
-    START_TXT = (
-        "<b>ALL MEDICAL VIDEOS AVAILABLE FREE\n"
-        "MARROW\n"
-        "PREPLADDER\n"
-        "DAMS\n"
-        "DOCTUT\n"
-        "MIST</b>"
-    )
+    START_TXT = """<b>ALL MEDICAL VIDEOS AVAILABLE FREE\n
+        MARROW\n
+        PREPLADDER\n
+        DAMS\n
+        DOCTUT\n
+        MIST</b>"""
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
