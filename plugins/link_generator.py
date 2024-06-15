10b5cfba37911a170e7826692972120ddb3f9da8
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-15 12:43:12 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -82,10 +82,10 @@ async def batch(client: Client, message: Message):
             
             # Send the caption followed by the link
             try:
-                await message.reply(f"*[{caption}]({link})*")
+                await message.reply(f"{caption}\n{link}")
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await message.reply(f"*[{caption}]({link})*")
+                await message.reply(f"\n{caption}{link}")
 
         except Exception as e:
             await message.reply(f"Error processing message {msg_id}: {e}")
