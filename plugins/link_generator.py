ea0e588c63507f383afbbabc6e134cefa71f3570
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-15 13:37:05 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,6 +5,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS, CUSTOM_CAPTION
 from helper_func import encode, get_message_id
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
