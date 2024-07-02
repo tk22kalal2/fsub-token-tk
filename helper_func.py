cb0b5b1e6cd93f01f0fdb3e2f8da982f5ae70bcc
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-02 12:51:24 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -1,3 +1,7 @@
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import asyncio
 import base64
 import re
@@ -10,7 +14,6 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
 
-X_CHANNEL = "-1002249946503"
 
 async def subschannel(filter, client, update):
     if not FORCE_SUB_CHANNEL:
@@ -19,11 +22,14 @@ async def subschannel(filter, client, update):
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id=X_CHANNEL, user_id=user_id)
+        member = await client.get_chat_member(
+            chat_id=FORCE_SUB_CHANNEL, user_id=user_id
+        )
     except UserNotParticipant:
         return False
 
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]
+
 
 async def subsgroup(filter, client, update):
     if not FORCE_SUB_GROUP:
@@ -32,11 +38,12 @@ async def subsgroup(filter, client, update):
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id=X_CHANNEL, user_id=user_id)
+        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
     except UserNotParticipant:
         return False
 
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]
+
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
@@ -47,11 +54,18 @@ async def is_subscribed(filter, client, update):
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id=X_CHANNEL, user_id=user_id)
+        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
+    except UserNotParticipant:
+        return False
+    try:
+        member = await client.get_chat_member(
+            chat_id=FORCE_SUB_CHANNEL, user_id=user_id
+        )
     except UserNotParticipant:
         return False
 
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]
+
 
 async def encode(string):
     string_bytes = string.encode("ascii")
@@ -60,11 +74,12 @@ async def encode(string):
     return base64_string
 
 async def decode(base64_string):
-    base64_string = base64_string.strip("=")
+    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
     string_bytes = base64.urlsafe_b64decode(base64_bytes) 
     string = string_bytes.decode("ascii")
     return string
+
 
 async def get_messages(client, message_ids):
     messages = []
@@ -72,18 +87,26 @@ async def get_messages(client, message_ids):
     while total_messages != len(message_ids):
         temb_ids = message_ids[total_messages : total_messages + 200]
         try:
-            msgs = await client.get_messages(chat_id=X_CHANNEL, message_ids=temb_ids)
+            msgs = await client.get_messages(
+                chat_id=client.db_channel.id, message_ids=temb_ids
+            )
         except FloodWait as e:
             await asyncio.sleep(e.x)
-            msgs = await client.get_messages(chat_id=X_CHANNEL, message_ids=temb_ids)
+            msgs = await client.get_messages(
+                chat_id=client.db_channel.id, message_ids=temb_ids
+            )
         except BaseException:
             pass
         total_messages += len(temb_ids)
         messages.extend(msgs)
     return messages
 
+
 async def get_message_id(client, message):
-    if (message.forward_from_chat and message.forward_from_chat.id == X_CHANNEL):
+    if (
+        message.forward_from_chat
+        and message.forward_from_chat.id == client.db_channel.id
+    ):
         return message.forward_from_message_id
     elif message.forward_from_chat or message.forward_sender_name or not message.text:
         return 0
@@ -95,10 +118,11 @@ async def get_message_id(client, message):
         channel_id = matches.group(1)
         msg_id = int(matches.group(2))
         if channel_id.isdigit():
-            if f"-100{channel_id}" == X_CHANNEL:
+            if f"-100{channel_id}" == str(client.db_channel.id):
                 return msg_id
-        elif channel_id == X_CHANNEL.username:
+        elif channel_id == client.db_channel.username:
             return msg_id
+
 
 subsgc = filters.create(subsgroup)
 subsch = filters.create(subschannel)
