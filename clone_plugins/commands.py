26284c23ff5aa40a4b7200069d075b3e8f545450
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-02 08:12:33 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -22,7 +22,7 @@ import base64
 from config import DB_URI as MONGO_URL
 from pymongo import MongoClient
 
-from helper_func import decode, get_messages, subsall, subsch, subsgc
+from clone_plugins.helper_func import decode, get_messages, subsall, subsch, subsgc
 from plugins.button import fsub_button, start_button
 mongo_client = MongoClient(MONGO_URL)
 mongo_db = mongo_client["cloned_vjbotz"]
