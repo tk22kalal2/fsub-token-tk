5ad4863e654b31af71ce80ccbc89c06027d503ff
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 00:42:11 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -2,13 +2,13 @@
 import re
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from bot import Bot
+from TechVJ.bot import StreamBot
 from config import ADMINS, CUSTOM_CAPTION, CD_CHANNEL
 from helper_func import encode, get_message_id
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 import asyncio
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -98,7 +98,7 @@ async def batch(client: Client, message: Message):
     await message.reply("Batch processing completed.")
     
     
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
