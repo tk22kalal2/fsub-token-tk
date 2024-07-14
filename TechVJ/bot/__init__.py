4bf63aac79fc78b94a6e88c2051072b7e9ba26b9
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 01:46:17 +0530

    Update __init__.py

--- TechVJ/bot/__init__.py
+++ TechVJ/bot/__init__.py
@@ -24,6 +24,7 @@ class StreamXBot(Client):
     def __init__(self):
         super().__init__(
             name="vjfiletolink",
+            db_channel=DB_CHANNEL,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
