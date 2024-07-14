a3a2445ac6c524d453575ba90ad9d223c2707d2c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 02:34:49 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -31,87 +31,10 @@ class CustomClient(Client):
             usr_bot_me = await self.get_me()
             self.username = usr_bot_me.username
             self.namebot = usr_bot_me.first_name
-            self.LOGGER(__name__).info(
-                f"TG_BOT_TOKEN detected!\n┌ First Name: {self.namebot}\n└ Username: @{self.username}\n——"
-            )
-        except Exception as a:
-            self.LOGGER(__name__).warning(a)
-            self.LOGGER(__name__).info(
-                "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
-            )
-            sys.exit()
-
-        if FORCE_SUB_CHANNEL:
-            try:
-                info = await self.get_chat(FORCE_SUB_CHANNEL)
-                link = info.invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                    link = info.invite_link
-                self.invitelink = link
-                self.LOGGER(__name__).info(
-                    f"FORCE_SUB_CHANNEL detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
-                )
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot can't Export Invite link from Force Sub Channel!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Make sure @{self.username} is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
-                )
-                sys.exit()
-
-        if FORCE_SUB_GROUP:
-            try:
-                info = await self.get_chat(FORCE_SUB_GROUP)
-                link = info.invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_GROUP)
-                    link = info.invite_link
-                self.invitelink2 = link
-                self.LOGGER(__name__).info(
-                    f"FORCE_SUB_GROUP detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
-                )
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot can't Export Invite link from Force Sub Group!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Make sure @{self.username} is Admin in group with Invite Users via Link Permission, Current Force Sub Group Value: {FORCE_SUB_GROUP}"
-                )
-                self.LOGGER(__name__).info(
-                    "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
-                )
-                sys.exit()
-
+            
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
             await test.delete()
-            self.LOGGER(__name__).info(
-                f"CHANNEL_ID Database detected!\n┌ Title: {db_channel.title}\n└ Chat ID: {db_channel.id}\n——"
-            )
-        except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(
-                f"Make sure @{self.username} is Admin in DB Channel, and double check the CHANNEL_ID value, Current Value: {CHANNEL_ID}"
-            )
-            self.LOGGER(__name__).info(
-                "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
-            )
-            sys.exit()
-
-        self.set_parse_mode(enums.ParseMode.HTML)
-        self.LOGGER(__name__).info(
-            f"[🔥 SUCCESSFULLY ACTIVATED! 🔥]"
-        )
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+            
