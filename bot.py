0e94ae7527fba0292ae5c5f62125b733e31024ef
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 00:07:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,8 +93,7 @@ async def start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    app = web.AppRunner(await web_server())
-    await StreamBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    app = web.AppRunner(await web_server())    
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, Var.PORT).start()
