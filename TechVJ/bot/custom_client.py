218c4b8c4093f91fc7e81a8595d2abefda74d017
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 02:44:34 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -5,12 +5,5 @@ from config import CHANNEL_ID
 
 class CustomClient(Client):
     def __init__(self, db_channel, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.db_channel = None
-
-    async def start(self):
-        await super().start()
-        db_channel = await self.get_chat(CHANNEL_ID)
         self.db_channel = db_channel
-        test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
-        await test.delete()
+        super().__init__(*args, **kwargs)
