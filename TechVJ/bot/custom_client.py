d59a696f3c1c34c650cdfac472e1fa58082e84b3
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 02:38:08 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -19,7 +19,6 @@ from config import (
     TG_BOT_WORKERS,
 )
 
-
 class CustomClient(Client):
     def __init__(self, db_channel, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -31,10 +30,15 @@ class CustomClient(Client):
             usr_bot_me = await self.get_me()
             self.username = usr_bot_me.username
             self.namebot = usr_bot_me.first_name
-            
-        try:
-            db_channel = await self.get_chat(CHANNEL_ID)
-            self.db_channel = db_channel
-            test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
-            await test.delete()
-            
+
+            try:
+                db_channel = await self.get_chat(CHANNEL_ID)
+                self.db_channel = db_channel
+                test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
+                await test.delete()
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+        except Exception as e:
+            print(f"An error occurred while starting the bot: {e}")
+
