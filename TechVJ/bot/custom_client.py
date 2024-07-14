9850dfa135ec28f98b42e62152b0029ac7620324
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-15 02:39:29 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -1,23 +1,7 @@
-# custom_client.py
-
 from pyrogram import Client
 from pyrogram.errors import FloodWait
 import asyncio
 from config import CHANNEL_ID
-import pyromod.listen
-import sys
-from pyrogram import enums
-
-from config import (
-    API_HASH,
-    APP_ID,
-    CHANNEL_ID,
-    FORCE_SUB_CHANNEL,
-    FORCE_SUB_GROUP,
-    LOGGER,
-    TG_BOT_TOKEN,
-    TG_BOT_WORKERS,
-)
 
 class CustomClient(Client):
     def __init__(self, db_channel, *args, **kwargs):
@@ -25,20 +9,8 @@ class CustomClient(Client):
         self.db_channel = db_channel
 
     async def start(self):
-        try:
-            await super().start()
-            usr_bot_me = await self.get_me()
-            self.username = usr_bot_me.username
-            self.namebot = usr_bot_me.first_name
-
-            try:
-                db_channel = await self.get_chat(CHANNEL_ID)
-                self.db_channel = db_channel
-                test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
-                await test.delete()
-            except Exception as e:
-                print(f"An error occurred: {e}")
-
-        except Exception as e:
-            print(f"An error occurred while starting the bot: {e}")
-
+        await super().start()
+        db_channel = await self.get_chat(CHANNEL_ID)
+        self.db_channel = db_channel
+        test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
+        await test.delete()
