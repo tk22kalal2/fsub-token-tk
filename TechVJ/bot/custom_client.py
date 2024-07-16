82fb8b3fee7eb16f8fafc9aee4af78b9fc9540f7
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-16 16:39:00 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -5,5 +5,6 @@ from config import CHANNEL_ID
 
 class CustomClient(Client):            
     def __init__(self, db_channel, *args, **kwargs):
+        db_channel = self.get_chat(CHANNEL_ID)
         self.db_channel = db_channel
         super().__init__(*args, **kwargs)
