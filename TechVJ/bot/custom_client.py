8d61951b139ae3e0a1f5d9afd0d0edca2765c22b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-16 16:29:48 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -7,13 +7,38 @@ class CustomClient(Client):
     def __init__(self, db_channel, *args, **kwargs):
         self.db_channel = db_channel
         super().__init__(*args, **kwargs)
+        
 
     async def start(self):
-        await super().start()
+        try:
+            await super().start()
+            usr_bot_me = await self.get_me()
+            self.username = usr_bot_me.username
+            self.namebot = usr_bot_me.first_name
+            self.LOGGER(__name__).info(
+                f"TG_BOT_TOKEN detected!\n┌ First Name: {self.namebot}\n└ Username: @{self.username}\n——"
+            )
+        except Exception as a:
+            self.LOGGER(__name__).warning(a)
+            self.LOGGER(__name__).info(
+                "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
+            )
+            sys.exit()
+
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id=db_channel.id, text="Test Message", disable_notification=True)
             await test.delete()
+            self.LOGGER(__name__).info(
+                f"CHANNEL_ID Database detected!\n┌ Title: {db_channel.title}\n└ Chat ID: {db_channel.id}\n——"
+            )
         except Exception as e:
-            print(f"An error occurred: {e}")
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(
+                f"Make Sure @{self.username} is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}"
+            )
+            self.LOGGER(__name__).info(
+                "Bot Stopped. Join https://t.me/CodeXBotzSupport for support"
+            )
+            sys.exit()
