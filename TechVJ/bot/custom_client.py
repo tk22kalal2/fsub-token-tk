2dc67ea0b88c763a90b9a6cb8fe50662b87df7a2
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-16 16:46:11 +0530

    Update custom_client.py

--- TechVJ/bot/custom_client.py
+++ TechVJ/bot/custom_client.py
@@ -3,8 +3,11 @@ from pyrogram.errors import FloodWait
 import asyncio
 from config import CHANNEL_ID
 
-class CustomClient(Client):            
-    def __init__(self, db_channel, *args, **kwargs):
-        db_channel = self.get_chat(CHANNEL_ID)
-        self.db_channel = db_channel
+class CustomClient(Client):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.db_channel = None
+
+    async def start(self):
+        await super().start()
+        self.db_channel = await self.get_chat(CHANNEL_ID)
