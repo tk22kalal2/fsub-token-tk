739e1b0e3a6dcc9771b94e9505d5eccf0649d0dd
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-17 22:58:00 +0530

    Update file_properties.py

--- TechVJ/utils/file_properties.py
+++ TechVJ/utils/file_properties.py
@@ -3,7 +3,7 @@ from typing import Any, Optional
 from pyrogram.types import Message
 from pyrogram.file_id import FileId
 from pyrogram.raw.types.messages import Messages
-from Adarsh.server.exceptions import FIleNotFound
+from TechVJ.server.exceptions import FIleNotFound
 
 
 async def parse_file_id(message: "Message") -> Optional[FileId]:
@@ -52,15 +52,8 @@ def get_hash(media_msg: Message) -> str:
 
 def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
-    file_name = getattr(media, 'file_name', "")
-    
-    # If file_name is absent, use the caption as a fallback
-    if not file_name and media_msg.caption:
-        return media_msg.caption.html
-    else:
-        return file_name
+    return getattr(media, 'file_name', "")
 
-        
 def get_media_file_size(m):
     media = get_media_from_message(m)
     return getattr(media, "file_size", 0)
