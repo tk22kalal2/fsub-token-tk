55253534bcce655da11255e3c25d6327b8042ac0
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-31 23:01:25 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -96,7 +96,7 @@ async def start(client, message):
         
         await client.send_message(
             chat_id=message.chat.id,
-            text=" ",
+            text=none
             reply_markup=reply_keyboard,
             protect_content=True
         )
