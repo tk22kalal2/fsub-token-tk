1c9445a6d6f339803eda769237b243135b03fe0c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-08-17 06:55:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,8 +9,7 @@ class script(object):
         PREPLADDER\n
         DAMS\n
         DOCTUT\n
-        MIST</b>
-    THESE IS STEP-3 TO WATCH LECTURE"""
+        MIST</b>\n\nTHESE IS STEP-3 TO WATCH LECTURE"""
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
